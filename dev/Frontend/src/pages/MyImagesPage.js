1eba5bd0444ae7ea74f0697ef2dc388cdaafc166
Nadavrav <nadavrav@post.bgu.ac.il>
2025-04-02 14:10:12 +0300

    Added viewimages and myimages pages

---
+++ dev/Frontend/src/pages/MyImagesPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const ViewImagesPage = () => {
+    return (
+        <div className="main-page">
+            <h1 className="main-title">Plan A</h1>
+            
+        </div>
+    );
+};
+
+export default ViewImagesPage;
