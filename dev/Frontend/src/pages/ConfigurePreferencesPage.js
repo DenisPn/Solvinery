1e3a817ee17e733e088ccc23e9806f856be373bd
Nadavrav <nadavrav@post.bgu.ac.il>
2025-04-02 23:28:52 +0300

    CVP Updates

--- dev/Frontend/src/pages/ConfigurePreferencesPage.js
+++ dev/Frontend/src/pages/ConfigurePreferencesPage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useZPL } from '../context/ZPLContext';
 import './ConfigureConstraintsPage.css';
+import '../Themes/MainTheme.css';
 
 const ConfigurePreferencesPage = () => {
     const navigate = useNavigate();
