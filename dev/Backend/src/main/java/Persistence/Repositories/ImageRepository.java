ec3f5f363b7bb3591f4ffde3903db1ee679f4d16
denis <panor@post.bgu.ac.il>
2025-06-19 17:39:43 +0300

    Added filters for user image search

--- dev/Backend/src/main/java/Persistence/Repositories/ImageRepository.java
+++ dev/Backend/src/main/java/Persistence/Repositories/ImageRepository.java
@@ -1,15 +1,18 @@
 package Persistence.Repositories;
 
 import Persistence.Entities.Image.ImageEntity;
+import Persistence.Entities.Image.PublishedImageEntity;
 import Persistence.Entities.UserEntity;
+import groupId.DTO.Records.Requests.Responses.ImagesDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
-public interface ImageRepository extends JpaRepository<ImageEntity, UUID> {
+public interface ImageRepository extends JpaRepository<ImageEntity, UUID>,
+                                        JpaSpecificationExecutor<ImageEntity> {
 
-    Page<ImageEntity> findByUser(UserEntity user, Pageable pageable);
 
 }
